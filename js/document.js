2ec7e3af3d470d92ba55947d2284b2b1ec7729a0
Yan Shan Hong <a8870506@gmail.com>
2021-05-15 13:32:49 +0800

    grwgw
    gwgweg

--- js/document.js
+++ js/document.js
@@ -1,22 +1,24 @@
-window.onload = function () { loadDoc() };
-
+window.onload = function () {
+     loadDoc() ;
+     loadfooter();
+};
 function loadDoc() {
 
     $.get("設定/Setting.txt", function (data) {
         let thisarry = data.split("\n");
 
         for (let index = 0; index < thisarry.length; index++) {
-            thisarry[index]=thisarry[index].replace(/^\s+|\s+$/g, '');
+            thisarry[index] = thisarry[index].replace(/^\s+|\s+$/g, '');
         }
         var schooltitle = thisarry.indexOf("0.顯示標題:") + 1;
-       
+
         document.title = thisarry[schooltitle];
 
         //輪播照片:標題
         let picTITLE1 = thisarry.indexOf("1.輪播標題1:") + 1;
         document.getElementById('pictxt ' + 1).innerHTML = thisarry[picTITLE1];
         document.getElementById('picsen ' + 1).innerHTML = thisarry[picTITLE1 + 1];
-       
+
         let picTITLE2 = thisarry.indexOf("2.輪播標題2:") + 1;
         document.getElementById('pictxt ' + 2).innerHTML = thisarry[picTITLE2];
         document.getElementById('picsen ' + 2).innerHTML = thisarry[picTITLE2 + 1];
@@ -49,45 +51,45 @@ function loadDoc() {
         }
         document.getElementById('Information ' + 2).innerHTML = Infocontent.join(' ');
 
-        //關於我們 資訊欄
-        // 1.電話:
-        let telenum = thisarry.indexOf("1.電話:") + 1;
-        document.getElementById('telephonenum').innerHTML = 'Tel：' + thisarry[telenum];
-        // 2.傳真:
-        let Faxnum = thisarry.indexOf("2.傳真:") + 1;
-        document.getElementById('Faxnum').innerHTML = 'Fax：' + thisarry[Faxnum];
-        // 3.Email:
-        let Emailnum = thisarry.indexOf("3.Email:") + 1;
-        document.getElementById('Emailnum').innerHTML = 'Email：' + thisarry[Emailnum];
-        // 4.服務時間:
-        let Servicetime = thisarry.indexOf("4.服務時間:") + 1;
-        document.getElementById('Servicetime').innerHTML = 'Service：' + thisarry[Servicetime];
+        // //關於我們 資訊欄
+        // // 1.電話:
+        // let telenum = thisarry.indexOf("1.電話:") + 1;
+        // document.getElementById('telephonenum').innerHTML = 'Tel：' + thisarry[telenum];
+        // // 2.傳真:
+        // let Faxnum = thisarry.indexOf("2.傳真:") + 1;
+        // document.getElementById('Faxnum').innerHTML = 'Fax：' + thisarry[Faxnum];
+        // // 3.Email:
+        // let Emailnum = thisarry.indexOf("3.Email:") + 1;
+        // document.getElementById('Emailnum').innerHTML = 'Email：' + thisarry[Emailnum];
+        // // 4.服務時間:
+        // let Servicetime = thisarry.indexOf("4.服務時間:") + 1;
+        // document.getElementById('Servicetime').innerHTML = 'Service：' + thisarry[Servicetime];
 
-        //社群相關連結網址
-        //1.FB:
-        let FBnum = thisarry.indexOf("1.FB:") + 1;
-        if (thisarry[FBnum] !== " ") {
-            document.getElementById('FBinfo').setAttribute("href", thisarry[FBnum]);
-        }
-        else {
-            document.getElementById('FBinfo').style.display = 'none';
-        }
-        //2.Instagram:
-        let IGnum = thisarry.indexOf("2.Instagram:") + 1;
-        if (thisarry[IGnum] !== " ") {
-            document.getElementById('IGinfo').setAttribute("href", thisarry[IGnum]);
-        }
-        else {
-            document.getElementById('IGinfo').style.display = 'none';
-        }
-        //2.Instagram:
-        let YTnum = thisarry.indexOf("3.Youtube:") + 1;
-        if (thisarry[YTnum] !== " ") {
-            document.getElementById('YTinfo').setAttribute("href", thisarry[YTnum]);
-        }
-        else {
-            document.getElementById('YTinfo').style.display = 'none';
-        }
+        // //社群相關連結網址
+        // //1.FB:
+        // let FBnum = thisarry.indexOf("1.FB:") + 1;
+        // if (thisarry[FBnum] !== " ") {
+        //     document.getElementById('FBinfo').setAttribute("href", thisarry[FBnum]);
+        // }
+        // else {
+        //     document.getElementById('FBinfo').style.display = 'none';
+        // }
+        // //2.Instagram:
+        // let IGnum = thisarry.indexOf("2.Instagram:") + 1;
+        // if (thisarry[IGnum] !== " ") {
+        //     document.getElementById('IGinfo').setAttribute("href", thisarry[IGnum]);
+        // }
+        // else {
+        //     document.getElementById('IGinfo').style.display = 'none';
+        // }
+        // //2.Instagram:
+        // let YTnum = thisarry.indexOf("3.Youtube:") + 1;
+        // if (thisarry[YTnum] !== " ") {
+        //     document.getElementById('YTinfo').setAttribute("href", thisarry[YTnum]);
+        // }
+        // else {
+        //     document.getElementById('YTinfo').style.display = 'none';
+        // }
     });
     //新聞填入
     $.get("News.txt", function (data) {
@@ -109,10 +111,5 @@ function loadDoc() {
     });
 
 }
-// function convertJson() {
-//     $.get("Setting.txt", function (data) {
-//         data.split('\n').map(function (el) {
-//              return el.split(/\s+/); });
-//     });
-// }
+
 
