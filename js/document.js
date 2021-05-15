588aa83834512f7c7fd7275443abb91f550d3a72
Yan Shan Hong <a8870506@gmail.com>
2021-05-15 13:54:39 +0800

    jss
    sjsj

--- js/document.js
+++ js/document.js
@@ -50,63 +50,28 @@ function loadDoc() {
             }
         }
         document.getElementById('Information ' + 2).innerHTML = Infocontent.join(' ');
-
-        // //關於我們 資訊欄
-        // // 1.電話:
-        // let telenum = thisarry.indexOf("1.電話:") + 1;
-        // document.getElementById('telephonenum').innerHTML = 'Tel：' + thisarry[telenum];
-        // // 2.傳真:
-        // let Faxnum = thisarry.indexOf("2.傳真:") + 1;
-        // document.getElementById('Faxnum').innerHTML = 'Fax：' + thisarry[Faxnum];
-        // // 3.Email:
-        // let Emailnum = thisarry.indexOf("3.Email:") + 1;
-        // document.getElementById('Emailnum').innerHTML = 'Email：' + thisarry[Emailnum];
-        // // 4.服務時間:
-        // let Servicetime = thisarry.indexOf("4.服務時間:") + 1;
-        // document.getElementById('Servicetime').innerHTML = 'Service：' + thisarry[Servicetime];
-
-        // //社群相關連結網址
-        // //1.FB:
-        // let FBnum = thisarry.indexOf("1.FB:") + 1;
-        // if (thisarry[FBnum] !== " ") {
-        //     document.getElementById('FBinfo').setAttribute("href", thisarry[FBnum]);
-        // }
-        // else {
-        //     document.getElementById('FBinfo').style.display = 'none';
-        // }
-        // //2.Instagram:
-        // let IGnum = thisarry.indexOf("2.Instagram:") + 1;
-        // if (thisarry[IGnum] !== " ") {
-        //     document.getElementById('IGinfo').setAttribute("href", thisarry[IGnum]);
-        // }
-        // else {
-        //     document.getElementById('IGinfo').style.display = 'none';
-        // }
-        // //2.Instagram:
-        // let YTnum = thisarry.indexOf("3.Youtube:") + 1;
-        // if (thisarry[YTnum] !== " ") {
-        //     document.getElementById('YTinfo').setAttribute("href", thisarry[YTnum]);
-        // }
-        // else {
-        //     document.getElementById('YTinfo').style.display = 'none';
-        // }
     });
     //新聞填入
     $.get("News.txt", function (data) {
         //填入文檔位置 : 文章標題
         var title = data.split("\n");
+
+        for (let index = 0; index < title.length; index++) {
+            title[index] = title[index].replace(/^\s+|\s+$/g, '');
+        }
         var content = title;
         document.getElementById('NewsTitle').innerHTML = title[0];
 
         //文章內容
         content.shift();
 
-        // //撇除img的物件
-        // var n = "</img>";
-
-        // content.filter(e => e !== n);
-        // console.log(content.includes(n));
-
+        //撇除img的物件
+        var n = "[圖片]";
+        for (let index = 0; index < title.length; index++) {
+            title[index] = title[index].replace(n, '');
+        }
+        //去除空白
+        content = content.filter(item => item);
         document.getElementById('Newscontent').innerHTML = content.join(' <br>');
     });
 
