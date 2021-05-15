d206fe46834dc98b608a3ef311da50608f1001c3
Yan Shan Hong <a8870506@gmail.com>
2021-05-15 21:07:11 +0800

    htrwjn
    swntjrwnrwn

--- js/SchoolInformation.js
+++ js/SchoolInformation.js
@@ -17,7 +17,41 @@ function Loadinfo() {
         // School level
         let LV = title.indexOf("School level") + 1;
         document.getElementById('SchoolLV').innerHTML = title[LV];
-        // // document.getElementById('NewsinnerTitle').innerHTML = title[0];
+
+        //Student grade level
+        let GLV = title.indexOf("Student grade level") + 1;
+        document.getElementById('SchoolGLV').innerHTML = title[GLV];
+
+        //Number of teaching staff & enrollment
+        let NTS = title.indexOf("Number of teaching staff & enrollment") + 1;
+        var Ntsstr = title[NTS] + '<br>' + title[NTS + 1];
+        document.getElementById('SchoolNTS').innerHTML = Ntsstr;
+
+        //Schools types
+        let ST = title.indexOf("Schools types") + 1;
+        document.getElementById('SchoolST').innerHTML = title[ST];
+
+        //Composition of students
+        let SOT = title.indexOf("Composition of students") + 1;
+        document.getElementById('SchoolSOT').innerHTML = title[SOT];
+
+        //Affiliation of schools
+        let AOS = title.indexOf("Affiliation of schools") + 1;
+        document.getElementById('SchoolAOS').innerHTML = title[AOS];
+
+        //The main instruction languages
+        let TMIL = title.indexOf("The main instruction languages") + 1;
+        document.getElementById('SchoolTMIL').innerHTML = title[TMIL];
+
+        //Foreign language programs
+        let FLP = title.indexOf("Foreign language programs") + 1;
+        document.getElementById('SchoolFLP').innerHTML = title[FLP];
+
+        //Number of overseas sister schools
+        let OSS = title.indexOf("Number of overseas sister schools") + 1;
+        title.splice(0,OSS);
+        console.log(title);
+        // document.getElementById('SchoolOSS').innerHTML = title[OSS];
 
         // //文章內容
         // content.shift();
@@ -44,7 +78,7 @@ function Loadinfo() {
 
                 let listfirstat = title.indexOf("*", 2 * index - 1);
                 title[listfirstat] = '<ul>';
-                title[listfirstat] = title[listfirstat] + '<br>';
+                title[listfirstat] = title[listfirstat] ;
                 let listfirend = title.indexOf("*", 2 * index);
                 title[listfirend] = '</ul>';
                 //列點項
@@ -56,7 +90,11 @@ function Loadinfo() {
             //尋找列點起始
 
         }
-        console.log(title);
+
+        for (let index = 0; index < title.length; index++) {
+            title[index] = title[index].replace(/^\s+|\s+$/g, '');
+        }
+         document.getElementById('SchoolOSS').innerHTML = title.split("\n");
         // document.getElementById('NewsinnerContent').innerHTML = content.join(' <br>');
     });
 }
