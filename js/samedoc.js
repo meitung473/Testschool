215d9f9aae6c9974dc46727f69c720f26f9afa6a
Yan Shan Hong <a8870506@gmail.com>
2021-05-15 19:20:04 +0800

    gfdgd
    gtrewgw

--- js/samedoc.js
+++ js/samedoc.js
@@ -8,15 +8,19 @@ function loadfooter() {
         // 1.電話:
         let telenum = thisarry.indexOf("1.電話:") + 1;
         document.getElementById('telephonenum').innerHTML = 'Tel：' + thisarry[telenum];
+        document.getElementById('telephonenum C').innerHTML = 'Fax：' + thisarry[telenum];
         // 2.傳真:
         let Faxnum = thisarry.indexOf("2.傳真:") + 1;
         document.getElementById('Faxnum').innerHTML = 'Fax：' + thisarry[Faxnum];
+        document.getElementById('Faxnum C').innerHTML = 'Fax：' + thisarry[Faxnum];
         // 3.Email:
         let Emailnum = thisarry.indexOf("3.Email:") + 1;
         document.getElementById('Emailnum').innerHTML = 'Email：' + thisarry[Emailnum];
+        document.getElementById('Emailnum C').innerHTML = 'Email：' + thisarry[Emailnum];
         // 4.服務時間:
         let Servicetime = thisarry.indexOf("4.服務時間:") + 1;
         document.getElementById('Servicetime').innerHTML = 'Service：' + thisarry[Servicetime];
+        document.getElementById('Servicetime C').innerHTML = 'Service：' + thisarry[Servicetime];
 
         //社群相關連結網址
         //1.FB:
