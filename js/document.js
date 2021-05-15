433617cda431db3d06d59d9021c6054333d036db
Yan Shan Hong <a8870506@gmail.com>
2021-05-15 10:53:00 +0800

    new
    new

--- js/document.js
+++ js/document.js
@@ -9,14 +9,14 @@ function loadDoc() {
             thisarry[index]=thisarry[index].replace(/^\s+|\s+$/g, '');
         }
         var schooltitle = thisarry.indexOf("0.顯示標題:") + 1;
-        console.log(schooltitle);
+       
         document.title = thisarry[schooltitle];
 
         //輪播照片:標題
         let picTITLE1 = thisarry.indexOf("1.輪播標題1:") + 1;
         document.getElementById('pictxt ' + 1).innerHTML = thisarry[picTITLE1];
         document.getElementById('picsen ' + 1).innerHTML = thisarry[picTITLE1 + 1];
-        console.log(picTITLE1);
+       
         let picTITLE2 = thisarry.indexOf("2.輪播標題2:") + 1;
         document.getElementById('pictxt ' + 2).innerHTML = thisarry[picTITLE2];
         document.getElementById('picsen ' + 2).innerHTML = thisarry[picTITLE2 + 1];
